028f0563f6ca8a28e68678119d731caee154a6dc
RiyaSingh-1915 <riya0719singh@gmail.com>
2025-06-10 20:11:14 +0000

    container config done

---
+++ 01_basic/03_dataType.js
@@ -0,0 +1,28 @@
+"use strict";//treat all JScode as newer version
+
+// alert(3+3)// we are using nodejs,not browser
+
+console.log(3
+    +  3
+)// code readability should be high
+
+let name ="riya"
+let age = 21
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint => larger numbers
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+// object
+
+console.log( typeof "riya");
+console.log( typeof 21);
+console.log( typeof false);
+console.log( typeof undefined);// undefined
+console.log( typeof null);//object
+
