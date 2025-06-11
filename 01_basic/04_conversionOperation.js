c40198c0930418dcee96bd0f1e0013cacf06d738
RiyaSingh-1915 <riya0719singh@gmail.com>
2025-06-11 12:33:01 +0000

    container config done

--- 01_basic/04_conversionOperation.js
+++ 01_basic/04_conversionOperation.js
@@ -1,25 +1,69 @@
-let score = true
-let name = "riya singh"
-console.log(typeof score );
-console.log(typeof (score));
+// let score = true
+// let name = "riya singh"
+// console.log(typeof score );
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0 
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// let isLoggedIn = 1
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
 // "riya" => true
 
-let someNumber = 33
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************** operations *********************
+
+let value = 3
+let negValue = -value
+ console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%2);
+
+// let str1 = "hello"
+// let str2 =  "riya"
+// let str3 = str1 +str2 
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + "2");
+// console.log("1" + 2 +2);
+// console.log(1 + 2 + "2");
+// console.log((3+4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 =num3 =2+2 
+
+// let gameCounter =  100
+// gameCounter++;
+// console.log(gameCounter);
+
+ let x = 3;
+ const y = x++;
+console.log(x:{x}, y:{y});
+// If used postfix, with operator after operand (for example, x++), the increment operator increments and returns the value before incrementing.
+let a = 3;
+const b = ++a;
+console.log(a :{a},b :{b});
+// If used prefix, with operator before operand (for example, ++x), the increment operator increments and returns the value after incrementing.
