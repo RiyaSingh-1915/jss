4583f2c1890a21b6fe641411d2d31c15233a07ce
RiyaSingh-1915 <riya0719singh@gmail.com>
2025-06-11 17:40:57 +0000

    container config done

---
+++ 01_basic/05_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+// comparision and equality check both are different 
+
+// === strict check 
+console.log("2" === 2);
