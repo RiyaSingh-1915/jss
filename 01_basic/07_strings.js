60d3049a8c1c6bd95dbee2da04b3b66830cfcf18
RiyaSingh-1915 <riya0719singh@gmail.com>
2025-06-13 09:40:59 +0000

    container config done

---
+++ 01_basic/07_strings.js
@@ -0,0 +1,38 @@
+// const name = "riya"
+// const repoCount = 50
+
+// console.log(name + repoCount + "Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+// const gameName = new String('hitesh-hc-com')
+
+// console.log(gameName[2]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.strike());
+// console.log(gameName.charAt(6));
+// console.log(gameName.indexOf('h'));
+
+// const newString = gameName.substring(0,5)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStringOne = "  riya  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.com/riya%20singh"
+
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('singh'))
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
+const str = "The quick brown fox jumps over the lazy dog.";
+const strCopy = str.split();
+console.log(strCopy);
+// Expected output: Array ["The quick brown fox jumps over the lazy dog."]
