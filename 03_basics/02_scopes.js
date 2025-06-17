a8be20edc0af61b12bc8f100a856978956114c74
RiyaSingh-1915 <riya0719singh@gmail.com>
2025-06-17 15:32:11 +0000

    container config done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -8,12 +8,49 @@ if(true){
     let a = 10
     const b = 20   //block scope
     // var c = 30
-    console.log("INNER:",a);
+    // console.log("INNER:",a);
 }
 
 
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+
+function one(){
+    const username = "riya"
+
+    function two(){
+        const website = "youtube"
+        console.log(username);
+    }
+    // console.log(website);
+
+    two()
+
+}
+// one()
+
+if(true) {
+    const username = "riya"
+    if (username === "riya"){
+        const website = "youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+//++++++++++++++++++++++++++++interesting+++++++++++++++++++++++++++ 
+ console.log(addOne(5))
+function addOne(num){
+    return num + 1
+}
+
+addTwo(5)
+const addTwo = function(num){          //variable hold also called hosting
+    return num +2
+}
 
