4c017fa7d04a39e1888bfd25dd52da993a9894a7
RiyaSingh-1915 <riya0719singh@gmail.com>
2025-06-24 20:54:35 +0530

    This is my first change

--- 01_practice/01_expense.js
+++ 01_practice/01_expense.js
@@ -3,7 +3,7 @@ const expense = [
 { item: "Pizza", amount: 5, catergory: "food", date:"2025-09-19"},
 { item: "Movie", amount: 2, catergory: "entertainment", date:"2025-09-11"},
 { item: "Hotel", amount: 2, catergory: "stay", date:"2025-07-19"},
-{ item: "Bus", amount: 2, catergory: "travel", date:"2025-04-19"}
+{ item: "car", amount: 2, catergory: "travel", date:"2025-04-19"}
 ]    
 
 let totalSpent = 0;
